7f17e6fd5a0f4b6ddecab6320d930ffc167c75e9
Szabolcs Nagy <szabolcs.nagy100@gmail.com>
2021-09-20 16:43:12 +0100

    Added Coroutines

---
+++ app/src/main/java/com/example/photoapitest/viewmodel/PhotoViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.photoapitest.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.example.photoapitest.model.remote.Network
+import com.example.photoapitest.model.remote.PhotoApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+private const val TAG = "PhotoViewModel"
+
+class PhotoViewModel: ViewModel() {
+    private val networkService: PhotoApi by lazy {
+        Network().api
+    }
+    /**
+     * Executes immediately after Constructor invocation
+     */
+    init {
+        // todo execute network call
+        // todo create the livedata
+        // todo update the livedata from coroutines response
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = networkService.getPhotos()
+            Log.d(TAG, "Coroutine Response: $response")        }
+    }
+}
